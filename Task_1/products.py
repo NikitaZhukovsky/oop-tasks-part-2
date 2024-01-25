432eb71bad5a1b91eab548873d23dadd24399240
NikitaZhukovsky <nikzuk2004@mail.ru>
2024-01-25 12:50:42 +0300

    New version

--- Task_1/products.py
+++ Task_1/products.py
@@ -1,17 +1,36 @@
 class Product:
     def __init__(self, name, store, price):
-        self.__name = name
-        self.__store = store
-        self.__price = price
+        if isinstance(name, str):
+            self.__name = name
+        else:
+            raise TypeError("Название товара должно быть строкой")
+        if isinstance(store, str):
+            self.__store = store
+        else:
+            raise TypeError("Название магазина должно быть строкой")
+        if isinstance(price, (int, float)) and price > 0:
+            self.__price = price
+        else:
+            raise ValueError("Цена должна быть положительным числом.")
 
-    def get_name(self):
+    @property
+    def name(self):
         return self.__name
 
-    def get_store(self):
+    @property
+    def store(self):
         return self.__store
 
-    def get_price(self):
+    @property
+    def price(self):
         return self.__price
+
+    @price.setter
+    def price(self, value):
+        if isinstance(value, (int, float)) and value > 0:
+            self.__price = value
+        else:
+            raise ValueError("Цена должна быть положительным числом.")
 
 
 class Warehouse:
@@ -26,25 +45,25 @@ class Warehouse:
 
     def get_product_by_name(self, name):
         for product in self.__products:
-            if product.get_name() == name:
+            if product.name == name:
                 return product
         return None
 
     def sort_by_name(self):
-        self.__products.sort(key=lambda product: product.get_name())
+        self.__products.sort(key=lambda product: product.name)
 
     def sort_by_store(self):
-        self.__products.sort(key=lambda product: product.get_store())
+        self.__products.sort(key=lambda product: product.store)
 
     def sort_by_price(self):
-        self.__products.sort(key=lambda product: product.get_price())
+        self.__products.sort(key=lambda product: product.price)
 
     def get_products(self):
         return self.__products
 
     def __add__(self, other):
-        total_price = sum([product.get_price() for product in self.__products])
-        total_price += sum([product.get_price() for product in other.__products])
+        total_price = sum([product.price for product in self.__products])
+        total_price += sum([product.price for product in other.__products])
         return total_price
 
 
@@ -52,16 +71,16 @@ product_one = Product("Телефон", "Магазин 1", 10000)
 product_two = Product("Ноутбук", "Магазин 2", 50000)
 product_three = Product("Машина", "Магазин 3", 2000000)
 
+product_one.price = 12000
 
 warehouse = Warehouse()
 warehouse.add_product(product_one)
 warehouse.add_product(product_two)
 warehouse.add_product(product_three)
 
-# Вывод информации о товарах
 print("Список товаров:")
 for index, product in enumerate(warehouse.get_products(), start=1):
-    print(f"Товар {index}: {product.get_name()}, {product.get_store()}, {product.get_price()} рублей")
+    print(f"Товар {index}: {product.name}, {product.store}, {product.price} рублей")
 
 state = True
 while state:
@@ -74,7 +93,7 @@ while state:
 
 product = warehouse.get_product_by_index(index)
 if product:
-    print(f"Товар {index + 1}: {product.get_name()}, {product.get_store()}, {product.get_price()} рублей")
+    print(f"Товар {index + 1}: {product.name}, {product.store}, {product.price} рублей")
 else:
     print("Товар не найден")
 
@@ -83,7 +102,7 @@ while state:
     name = input("Введите название товара для поиска: ")
     product = warehouse.get_product_by_name(name)
     if product:
-        print(f"Товар '{name}': {product.get_store()}, {product.get_price()} рублей")
+        print(f"Товар '{name}': {product.store}, {product.price} рублей")
         state = False
     else:
         print("Товар не найден. Попробуйте ещё раз ввести название товара")
@@ -92,17 +111,17 @@ while state:
 warehouse.sort_by_name()
 print("Товары, отсортированные по названию:")
 for product in warehouse.get_products():
-    print(f"{product.get_name()}, {product.get_store()}, {product.get_price()} рублей")
+    print(f"{product.name}, {product.store}, {product.price} рублей")
 
 warehouse.sort_by_store()
 print("Товары, отсортированные по магазину:")
 for product in warehouse.get_products():
-    print(f"{product.get_name()}, {product.get_store()}, {product.get_price()} рублей")
+    print(f"{product.name}, {product.store}, {product.price} рублей")
 
 warehouse.sort_by_price()
 print("Товары, отсортированные по цене:")
 for product in warehouse.get_products():
-    print(f"{product.get_name()}, {product.get_store()}, {product.get_price()} рублей")
+    print(f"{product.name}, {product.store}, {product.price} рублей")
 
 warehouse_two = Warehouse()
 print("Добавлены новые товары: ")
@@ -110,7 +129,7 @@ warehouse_two.add_product(Product("Вилка", "Магазин 2", 20))
 warehouse_two.add_product(Product("Книга", "Магазин 1", 300))
 
 for index, product in enumerate(warehouse_two.get_products(), start=1):
-    print(f"Товар {index}: {product.get_name()}, {product.get_store()}, {product.get_price()} рублей")
+    print(f"Товар {index}: {product.name}, {product.store}, {product.price} рублей")
 
 total_price = warehouse + warehouse_two
 print(f"Общая стоимость товаров на складе: {total_price} рублей")
